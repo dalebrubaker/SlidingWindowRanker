be21d2f52d4987e24450206e3a1cee9a98881e4e
Dale Brubaker <brubaker.dale@gmail.com>
2024-09-17 07:50:40 -0400

    Add GetLowerBound tests

--- SlidingWindowRanker.Tests/CollectionExtensionTests.cs
+++ SlidingWindowRanker.Tests/CollectionExtensionTests.cs
@@ -33,6 +33,63 @@ public class CollectionExtensionTests
         var result = list.GetLowerBound(3);
         Assert.Equal(2, result);
     }
+    
+    
+    [Fact]
+    public void GetLowerBound_ShouldReturnCorrectIndex_WhenElementExists()
+    {
+        // Arrange
+        var list = new List<int> { 1, 3, 5, 7, 9 };
+        var value = 5;
+
+        // Act
+        var result = list.GetLowerBound(0, list.Count, value);
+
+        // Assert
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void GetLowerBound_ShouldReturnCorrectIndex_WhenElementDoesNotExist()
+    {
+        // Arrange
+        var list = new List<int> { 1, 3, 5, 7, 9 };
+        var value = 6;
+
+        // Act
+        var result = list.GetLowerBound(0, list.Count, value);
+
+        // Assert
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void GetLowerBound_ShouldReturnZero_WhenValueIsLessThanAllElements()
+    {
+        // Arrange
+        var list = new List<int> { 1, 3, 5, 7, 9 };
+        var value = 0;
+
+        // Act
+        var result = list.GetLowerBound(0, list.Count, value);
+
+        // Assert
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void GetLowerBound_ShouldReturnCount_WhenValueIsGreaterThanAllElements()
+    {
+        // Arrange
+        var list = new List<int> { 1, 3, 5, 7, 9 };
+        var value = 10;
+
+        // Act
+        var result = list.GetLowerBound(0, list.Count, value);
+
+        // Assert
+        Assert.Equal(list.Count, result);
+    }
 
     [Fact]
     public void GetUpperBound_ShouldReturnCorrectIndex()
