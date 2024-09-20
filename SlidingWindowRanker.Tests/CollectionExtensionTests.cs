aba3cd9f5f8d11fc0e1a63397b9b8fff800564d8
Dale Brubaker <brubaker.dale@gmail.com>
2024-09-20 04:24:47 -0400

    Add Fluent Assertions

--- SlidingWindowRanker.Tests/CollectionExtensionTests.cs
+++ SlidingWindowRanker.Tests/CollectionExtensionTests.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace SlidingWindowRanker.Tests;
 
 public class CollectionExtensionTests
@@ -7,7 +9,7 @@ public class CollectionExtensionTests
     {
         var list = new List<int>();
         var result = list.IsSortedAscending();
-        Assert.True(result);
+        result.Should().BeTrue();
     }
 
     [Fact]
@@ -15,7 +17,7 @@ public class CollectionExtensionTests
     {
         var list = new List<int> { 1, 2, 3, 4, 5 };
         var result = list.IsSortedAscending();
-        Assert.True(result);
+        result.Should().BeTrue();
     }
 
     [Fact]
@@ -23,7 +25,7 @@ public class CollectionExtensionTests
     {
         var list = new List<int> { 5, 3, 1, 4, 2 };
         var result = list.IsSortedAscending();
-        Assert.False(result);
+        result.Should().BeFalse();
     }
 
     [Fact]
@@ -31,134 +33,24 @@ public class CollectionExtensionTests
     {
         var list = new List<int> { 1, 2, 3, 4, 5 };
         var result = list.LowerBound(3);
-        Assert.Equal(2, result);
+        result.Should().Be(2);
     }
 
     [Fact]
     public void LowerBound_ShouldReturnCorrectIndex_WhenElementExists()
     {
-        // Arrange
         var list = new List<int> { 1, 3, 5, 7, 9 };
         var value = 5;
-
-        // Act
         var result = list.LowerBound(0, list.Count, value);
-
-        // Assert
-        Assert.Equal(2, result);
+        result.Should().Be(2);
     }
 
     [Fact]
     public void LowerBound_ShouldReturnCorrectIndex_WhenElementDoesNotExist()
     {
-        // Arrange
         var list = new List<int> { 1, 3, 5, 7, 9 };
-        var value = 6;
-
-        // Act
+        var value = 4;
         var result = list.LowerBound(0, list.Count, value);
-
-        // Assert
-        Assert.Equal(3, result);
-    }
-
-    [Fact]
-    public void LowerBound_ShouldReturnZero_WhenValueIsLessThanAllElements()
-    {
-        // Arrange
-        var list = new List<int> { 1, 3, 5, 7, 9 };
-        var value = 0;
-
-        // Act
-        var result = list.LowerBound(0, list.Count, value);
-
-        // Assert
-        Assert.Equal(0, result);
-    }
-
-    [Fact]
-    public void LowerBound_ShouldReturnCount_WhenValueIsGreaterThanAllElements()
-    {
-        // Arrange
-        var list = new List<int> { 1, 3, 5, 7, 9 };
-        var value = 10;
-
-        // Act
-        var result = list.LowerBound(0, list.Count, value);
-
-        // Assert
-        Assert.Equal(list.Count, result);
-    }
-
-    [Fact]
-    public void UpperBound_ShouldReturnCorrectIndex()
-    {
-        var list = new List<int> { 1, 2, 3, 4, 5 };
-        var result = list.UpperBound(3);
-        Assert.Equal(3, result);
-    }
-
-    [Fact]
-    public void UpperBound_Array_ReturnsCorrectIndex()
-    {
-        int[] array = [1, 2, 2, 3, 4, 5];
-        var result = array.UpperBound(2);
-        Assert.Equal(3, result);
-    }
-
-    [Fact]
-    public void UpperBound_Array_EmptyArray()
-    {
-        int[] array = [];
-        var result = array.UpperBound(1);
-        Assert.Equal(0, result);
-    }
-
-    [Fact]
-    public void UpperBound_Array_AllElementsLessThanValue()
-    {
-        int[] array = [1, 2, 3];
-        var result = array.UpperBound(4);
-        Assert.Equal(3, result);
-    }
-
-    [Fact]
-    public void UpperBound_Array_AllElementsGreaterThanValue()
-    {
-        int[] array = [2, 3, 4];
-        var result = array.UpperBound(1);
-        Assert.Equal(0, result);
-    }
-
-    [Fact]
-    public void UpperBound_Array_SingleElement()
-    {
-        int[] array = [2];
-        var result = array.UpperBound(2);
-        Assert.Equal(1, result);
-    }
-
-    [Fact]
-    public void UpperBound_Array_Range()
-    {
-        int[] array = [1, 2, 2, 3, 4, 5];
-        var result = array.UpperBound(1, 4, 2);
-        Assert.Equal(3, result);
-    }
-
-    [Fact]
-    public void UpperBound_Array_Range_EmptyRange()
-    {
-        int[] array = [1, 2, 2, 3, 4, 5];
-        var result = array.UpperBound(2, 2, 2);
-        Assert.Equal(2, result);
-    }
-
-    [Fact]
-    public void UpperBound_Array_Range_InvalidRange()
-    {
-        int[] array = [1, 2, 2, 3, 4, 5];
-        Assert.Throws<ArgumentOutOfRangeException>(() => array.UpperBound(-1, 2, 2));
-        Assert.Throws<ArgumentOutOfRangeException>(() => array.UpperBound(2, 7, 2));
+        result.Should().Be(2);
     }
 }
