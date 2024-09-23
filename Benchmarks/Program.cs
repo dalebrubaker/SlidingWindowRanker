c1362b946e67d34de7fa03bc966edbfc0376325f
Dale Brubaker <brubaker.dale@gmail.com>
2024-09-23 08:49:41 -0400

    Remove dead code

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,15 +1,12 @@
 ﻿using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 using Benchmarks;
-// ReSharper disable ExpressionIsAlwaysNull
 
 // ReSharper disable once JoinDeclarationAndInitializer
 DebugInProcessConfig config;
 #if DEBUG
 config = new DebugInProcessConfig();
-#else 
+#else
 config = null;
 #endif
-//var summaryLowerBound = BenchmarkRunner.Run<BenchmarkLowerBound>();
-//var summaryUpperBound = BenchmarkRunner.Run<BenchmarkUpperBound>();
 var summarySlidingWindowRanker = BenchmarkRunner.Run<BenchmarkSlidingWindowRanker>(config);
